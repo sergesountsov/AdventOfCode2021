3b41b0e5576fa301b0a293a9491edb99e5af8f55
Serge Sountsov <serge.sountsov@gmail.com>
2021-12-01 11:16:01 -0800

    Day1/part1 done

--- day01/part1/Program.cs
+++ day01/part1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int increaseCount =0;
+int previous =int.MaxValue;
+string? line;
+
+while ((line = Console.ReadLine()) != null) {
+    var current = int.Parse(line);
+    if (current > previous ) {
+        increaseCount++;
+    }
+    previous = current;
+}
+
+Console.WriteLine("{0} increases", increaseCount);
