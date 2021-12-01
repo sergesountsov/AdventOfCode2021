e6f9861078d1f3984aca73450ef6ed996e8f5f7b
Serge Sountsov <serge.sountsov@gmail.com>
2021-12-01 11:33:55 -0800

    part 2 done

---
+++ day01/part2/Program.cs
@@ -0,0 +1,18 @@
+﻿int increaseCount =0;
+int previous =int.MaxValue;
+string? line;
+List<int> measurements = new();
+
+while ((line = Console.ReadLine()) != null) {
+    measurements.Add(int.Parse(line));
+}
+
+for (int i =0; i < measurements.Count - 2; i++) {
+    int current = measurements[i]+measurements[i+1]+measurements[i+2];
+    if (current > previous) {
+        increaseCount++;
+    }
+    previous = current;
+}
+
+Console.WriteLine("{0} increases", increaseCount);
