3c6c9bcae767a091b47ae509289296cfcb615517
Serge Sountsov <serge.sountsov@gmail.com>
2021-12-02 16:36:12 -0800

    Day 2 Part 1

---
+++ day02/part1/Program.cs
@@ -0,0 +1,17 @@
+﻿string? line;
+int distance = 0;
+int depth = 0 ;
+
+while ((line = Console.ReadLine()) != null) {
+    string[] parts = line.Split(" ");
+    int arg = int.Parse(parts[1]);
+    switch (parts[0]) {
+        case "up": depth -= arg; break;
+        case "down": depth += arg; break;
+        case "forward": distance += arg; break;
+        default: throw new ArgumentException($"Unexpected command {parts[0]}"); 
+
+    }
+}
+
+Console.WriteLine ("distance: {0}, depth: {1}, answer: {2}", distance, depth, distance*depth);
