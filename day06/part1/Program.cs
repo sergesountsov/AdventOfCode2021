3be7d0c9236f4b0de9baeed538322e389a18a835
sergesountsov <serge.sountsov@gmail.com>
2021-12-06 05:54:46 +0000

    Day 6 part 1

---
+++ day06/part1/Program.cs
@@ -0,0 +1,33 @@
+﻿int[] input = {2,5,3,4,4,5,3,2,3,3,2,2,4,2,5,4,1,1,4,4,5,1,2,1,5,2,1,5,1,1,1,2,4,3,3,1,4,2,3,4,5,1,2,5,1,2,2,5,2,4,4,1,4,5,4,2,1,5,5,3,2,1,3,2,1,4,2,5,5,5,2,3,3,5,1,1,5,3,4,2,1,4,4,5,4,5,3,1,4,5,1,5,3,5,4,4,4,1,4,2,2,2,5,4,3,1,4,4,3,4,2,1,1,5,3,3,2,5,3,1,2,2,4,1,4,1,5,1,1,2,5,2,2,5,2,4,4,3,4,1,3,3,5,4,5,4,5,5,5,5,5,4,4,5,3,4,3,3,1,1,5,2,4,5,5,1,5,2,4,5,4,2,4,4,4,2,2,2,2,2,3,5,3,1,1,2,1,1,5,1,4,3,4,2,5,3,4,4,3,5,5,5,4,1,3,4,4,2,2,1,4,1,2,1,2,1,5,5,3,4,1,3,2,1,4,5,1,5,5,1,2,3,4,2,1,4,1,4,2,3,3,2,4,1,4,1,4,4,1,5,3,1,5,2,1,1,2,3,3,2,4,1,2,1,5,1,1,2,1,2,1,2,4,5,3,5,5,1,3,4,1,1,3,3,2,2,4,3,1,1,2,4,1,1,1,5,4,2,4,3};
+
+int[] fishCount = new int[9];
+
+foreach (var fish in input) {
+    fishCount[fish] +=1;
+}
+
+PrintFish(fishCount);
+for(int step = 1; step <= 80; step++) {
+    Step(fishCount);
+    Console.WriteLine($"Step {step}");
+    PrintFish(fishCount);
+}
+
+var totalFish = fishCount.Aggregate(0,(count,next) => count + next);
+Console.WriteLine ($"Part1: {totalFish}");
+
+void PrintFish(int[] fishCount) {
+    for  (int i =0; i < fishCount.Length; i++) {
+        Console.WriteLine($"{i}: {fishCount[i]}");
+    }
+}
+
+void Step(int[] fishCount) {
+    int fishAtZero = fishCount[0];
+
+    for (int i = 1; i < fishCount.Length; i++) {
+        fishCount[i-1]=fishCount[i];
+    }
+    fishCount[6] += fishAtZero;
+    fishCount[8] = fishAtZero;
+ }
