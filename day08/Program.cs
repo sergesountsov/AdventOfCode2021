1643f5e1f2c641f3f5325614bd84ea7f07d2a77e
sergesountsov <serge.sountsov@gmail.com>
2021-12-08 05:48:34 +0000

    Day 8 part 1

---
+++ day08/Program.cs
@@ -0,0 +1,13 @@
+﻿string? line;
+int result =0;
+while ((line = Console.ReadLine())!=null) {
+    var parts = line.Split(" | ");
+    var outputs = parts[1].Split(' ');
+    foreach (var o in outputs) {
+        if (o.Length == 2 || o.Length ==3 ||o.Length ==4 || o.Length == 7) {
+            result++;
+        }
+    }
+}
+
+Console.WriteLine($"Part1 result: {result} ");
