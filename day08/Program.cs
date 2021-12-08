7c4424c28b6db95231a70b108a25c91d02e55cc1
sergesountsov <serge.sountsov@gmail.com>
2021-12-08 07:04:55 +0000

    refactor for part 2, add seach logic thoughts

--- day08/Program.cs
+++ day08/Program.cs
@@ -1,13 +1,22 @@
-﻿string? line;
-int result =0;
-while ((line = Console.ReadLine())!=null) {
-    var parts = line.Split(" | ");
-    var outputs = parts[1].Split(' ');
+﻿
+int FindResult (Func<string[], string[], int> decode) {
+    string? line;
+    int result =0;
+    while ((line = Console.ReadLine())!=null) {
+        var parts = line.Split(" | ");
+        var outputs = parts[1].Split(' ');
+        result += decode(parts[0].Split(' '), parts[1].Split(' '));
+    }
+    return result;
+}
+
+int Part1Decode (string[] segments, string[] outputs) {
+    int result =0;
     foreach (var o in outputs) {
         if (o.Length == 2 || o.Length ==3 ||o.Length ==4 || o.Length == 7) {
             result++;
         }
     }
+    return result;
 }
-
-Console.WriteLine($"Part1 result: {result} ");
+Console.WriteLine($"Part1 result: {FindResult(Part1Decode)}");
